051d655d60f2129dc2eb2fb9b851043ddbd6d071
Tek Yin <kwee.yin@gojek.com>
2022-09-23 00:40:30 +0700

    separate CSS and refactor

--- main.js
+++ main.js
@@ -3,30 +3,30 @@
  */
 
 var dpr, rc, ctx;
-const DEBUG = false;
+const DEBUG = true;
 const STORAGE_KEY = 'kafka-streams-viz';
 
 function processName(name) {
-	return name.replace(/-/g, '-\\n');
+    return name.replace(/-/g, '-\\n');
 }
 
 // converts kafka stream ascii topo description to DOT language
 function convertTopoToDot(topo) {
-	var lines = topo.split('\n');
-	var results = [];
-	var outside = [];
-	var stores = new Set();
-	var topics = new Set();
-	var entityName;
+    var lines = topo.split('\n');
+    var results = [];
+    var outside = [];
+    var stores = new Set();
+    var topics = new Set();
+    var entityName;
 
-	// dirty but quick parsing
-	lines.forEach(line => {
-		var sub = /Sub-topology: (\w*)/;
-		var match = sub.exec(line);
+    // dirty but quick parsing
+    lines.forEach(line => {
+        var sub = /Sub-topology: (\w*)/;
+        var match = sub.exec(line);
 
-		if (match) {
-			if (results.length) results.push(`}`);
-			results.push(`subgraph cluster_${match[1]} {
+        if (match) {
+            if (results.length) results.push(`}`);
+            results.push(`subgraph cluster_${match[1]} {
 			label = "${match[0]}";
 
 			style=filled;
@@ -34,140 +34,135 @@ function convertTopoToDot(topo) {
 			node [style=filled,color=white];
 			`);
 
-			return;
-		}
+            return;
+        }
 
-		match = /(Source\:|Processor\:|Sink:)\s+(\S+)\s+\((topics|topic|sourceTopic|stores)\:(.*)\)/.exec(line)
+        match = /(Source\:|Processor\:|Sink:)\s+(\S+)\s+\((topics|topic|sourceTopic|stores)\:(.*)\)/.exec(line)
 
-		if (match) {
-			entityName = processName(match[2]);
-			const entityType = match[1];
-			var type = match[3]; // source, processor or sink
-			var linkedNames = match[4];
-			linkedNames = linkedNames.replace(/\[|\]/g, '');
-			linkedNames.split(',').forEach(linkedName => {
-				linkedName = processName(linkedName.trim());
+        if (match) {
+            entityName = processName(match[2]);
+            const entityType = match[1];
+            var type = match[3]; // source, processor or sink
+            var linkedNames = match[4];
+            linkedNames = linkedNames.replace(/\[|\]/g, '');
+            linkedNames.split(',').forEach(linkedName => {
+                linkedName = processName(linkedName.trim());
 
-				if (linkedName === '') {
-					// short circuit
-				}
-				else if (type === 'topics') {
-					// from
-					outside.push(`"${linkedName}" -> "${entityName}";`);
-					topics.add(linkedName);
-				}
-				else if (type === 'topic') {
-					// to
+                if (linkedName === '') {
+                    // short circuit
+                } else if (type === 'topics') {
+                    // from
+                    outside.push(`"${linkedName}" -> "${entityName}";`);
+                    topics.add(linkedName);
+                } else if (type === 'topic') {
+                    // to
                     if (entityType === 'Sink:') {
                         outside.push(`"${entityName}" -> "${linkedName}";`);
                     } else {
                         results.push(`"${entityName}" -> "${linkedName}";`);
                     }
                     topics.add(linkedName);
-				}
-				else if (type === 'stores') {
-					if (entityName.includes("JOIN") || entityType === 'Source:') {
+                } else if (type === 'stores') {
+                    if (entityName.includes("JOIN") || entityType === 'Source:') {
                         outside.push(`"${linkedName}" -> "${entityName}";`);
                     } else if (entityType === 'Processor:') {
                         results.push(`"${entityName}" -> "${linkedName}";`);
-					} else {
-						outside.push(`"${entityName}" -> "${linkedName}";`);
-					}
+                    } else {
+                        outside.push(`"${entityName}" -> "${linkedName}";`);
+                    }
 
-					stores.add(linkedName);
-				}
-                else if (type === 'sourceTopic') {
+                    stores.add(linkedName);
+                } else if (type === 'sourceTopic') {
                     if (entityType === 'Sink') {
                         outside.push(`"${linkedName}" -> "${entityName}";`);
-                    }
-                    else {
+                    } else {
                         results.push(`"${linkedName}" -> "${entityName}";`);
                     }
                     topics.add(linkedName);
                 }
-			});
+            });
 
-			return;
-		}
+            return;
+        }
 
-		match = /\-\-\>\s+(.*)$/.exec(line);
+        match = /\-\-\>\s+(.*)$/.exec(line);
 
-		if (match && entityName) {
-			var targets = match[1];
-			targets.split(',').forEach(name => {
-				var linkedName = processName(name.trim());
-				if (linkedName === 'none') return;
+        if (match && entityName) {
+            var targets = match[1];
+            targets.split(',').forEach(name => {
+                var linkedName = processName(name.trim());
+                if (linkedName === 'none') return;
 
-				results.push(`"${entityName}" -> "${linkedName}";`);
-			});
-		}
-	})
+                results.push(`"${entityName}" -> "${linkedName}";`);
+            });
+        }
+    })
 
-	if (results.length) results.push(`}`);
+    if (results.length) results.push(`}`);
 
-	results = results.concat(outside);
+    results = results.concat(outside);
 
-	stores.forEach(node => {
-		results.push(`"${node}" [shape=cylinder, color=black, fillcolor=lightgrey];`)
-	});
+    stores.forEach(node => {
+        results.push(`"${node}" [shape=cylinder, color=black, fillcolor=lightgrey];`)
+    });
 
-	topics.forEach(node => {
-		results.push(`"${node}" [shape=rect, color=black, fillcolor=lightgrey];`)
-	});
+    topics.forEach(node => {
+        results.push(`"${node}" [shape=rect, color=black, fillcolor=lightgrey];`)
+    });
 
-	return `
+    let graph = `
 	digraph G {
 		label = "Kafka Streams Topology"
 
 		${results.join('\n')}
 	}
 	`;
+    console.log(graph);
+    return graph;
 }
 
 window.update = function () {
-	console.log("update topology");
-	var topo = input.value;
-	var dotCode = convertTopoToDot(topo);
-	if (DEBUG) console.log('dot code\n', dotCode);
+    console.log("update topology");
+    var topo = input.value;
+    var dotCode = convertTopoToDot(topo);
+    if (DEBUG) console.log('dot code\n', dotCode);
 
+    graphviz_code.value = dotCode;
 
-	graphviz_code.value = dotCode;
+    var params = {
+        engine: 'dot',
+        format: 'svg'
+    };
 
-	var params = {
-		engine: 'dot',
-		format: 'svg'
-  	};
+    const svgCode = Viz(dotCode, params);
 
-	var svgCode = Viz(dotCode, params);
+    svg_container.innerHTML = svgCode;
 
-	svg_container.innerHTML = svgCode;
+    const svg = svg_container.querySelector('svg');
+    dpr = window.devicePixelRatio
+    canvas.width = svg.viewBox.baseVal.width * dpr | 0;
+    canvas.height = svg.viewBox.baseVal.height * dpr | 0;
+    canvas.style.width = `${svg.viewBox.baseVal.width}px`;
+    canvas.style.height = `${svg.viewBox.baseVal.height}px`;
 
-	var svg = svg_container.querySelector('svg')
-	dpr = window.devicePixelRatio
-	canvas.width = svg.viewBox.baseVal.width * dpr | 0;
-	canvas.height = svg.viewBox.baseVal.height * dpr | 0;
-	canvas.style.width = `${svg.viewBox.baseVal.width}px`;
-	canvas.style.height = `${svg.viewBox.baseVal.height}px`;
+    rc = rough.canvas(canvas);
+    ctx = rc.ctx
+    ctx.scale(dpr, dpr);
 
-	rc = rough.canvas(canvas);
-	ctx = rc.ctx
-	ctx.scale(dpr, dpr);
+    const g = svg.querySelector('g');
 
-	var g = svg.querySelector('g');
+    try {
+        traverseSvgToRough(g);
 
-	try {
-		traverseSvgToRough(g);
-
-		sessionStorage.setItem(STORAGE_KEY, topo);
-	}
-	catch (e) {
-		console.error('Exception generating graph', e && e.stack || e);
-		// TODO update Frontend
-	}
+        sessionStorage.setItem(STORAGE_KEY, topo);
+    } catch (e) {
+        console.error('Exception generating graph', e && e.stack || e);
+        // TODO update Frontend
+    }
 }
 
 function nullIfNone(attribute) {
-	return attribute === 'none' ? null : attribute;
+    return attribute === 'none' ? null : attribute;
 }
 
 /**
@@ -175,125 +170,126 @@ function nullIfNone(attribute) {
  */
 
 function getFillStroke(child) {
-	var fill = nullIfNone(child.getAttribute('fill'));
-	var stroke = nullIfNone(child.getAttribute('stroke'));
-	var isBaseRectangle = child.nodeName === 'polygon' && child.parentNode.id === 'graph0';
+    var fill = nullIfNone(child.getAttribute('fill'));
+    var stroke = nullIfNone(child.getAttribute('stroke'));
+    var isBaseRectangle = child.nodeName === 'polygon' && child.parentNode.id === 'graph0';
 
-	return {
-		fill: isBaseRectangle ? 'white' : fill,
-		fillStyle: isBaseRectangle ? 'solid' : 'hachure',
-		stroke: stroke
-	};
+    return {
+        fill: isBaseRectangle ? 'white' : fill,
+        fillStyle: isBaseRectangle ? 'solid' : 'hachure',
+        stroke: stroke
+    };
 }
 
 function splitToArgs(array, delimiter) {
-	return array.split(delimiter || ',').map(v => +v);
+    return array.split(delimiter || ',').map(v => +v);
 }
 
 // node traversal function
 function traverseSvgToRough(child) {
 
-	if (child.nodeName === 'path') {
-		var d = child.getAttribute('d');
-		var opts = getFillStroke(child);
-		rc.path(d, opts);
-		return;
-	  }
+    if (child.nodeName === 'path') {
+        var d = child.getAttribute('d');
+        var opts = getFillStroke(child);
+        rc.path(d, opts);
+        return;
+    }
 
-	if (child.nodeName === 'ellipse') {
-		var cx = +child.getAttribute('cx');
-		var cy = +child.getAttribute('cy');
-		var rx = +child.getAttribute('rx');
-		var ry = +child.getAttribute('ry');
+    if (child.nodeName === 'ellipse') {
+        var cx = +child.getAttribute('cx');
+        var cy = +child.getAttribute('cy');
+        var rx = +child.getAttribute('rx');
+        var ry = +child.getAttribute('ry');
 
-		var opts = getFillStroke(child);
+        var opts = getFillStroke(child);
 
-		rc.ellipse(cx, cy, rx * 1.5, ry * 1.5);
-		return;
-  	}
+        rc.ellipse(cx, cy, rx * 1.5, ry * 1.5);
+        return;
+    }
 
-	if (child.nodeName === 'text') {
-		var fontFamily = child.getAttribute('font-family')
-		var fontSize = +child.getAttribute('font-size')
-		var anchor = child.getAttribute('text-anchor')
+    if (child.nodeName === 'text') {
+        var fontFamily = child.getAttribute('font-family')
+        var fontSize = +child.getAttribute('font-size')
+        var anchor = child.getAttribute('text-anchor')
 
-		if (anchor === 'middle') {
-			ctx.textAlign = 'center';
-		}
+        if (anchor === 'middle') {
+            ctx.textAlign = 'center';
+        }
 
-		if (fontFamily) {
-			ctx.fontFamily = fontFamily;
-		}
+        if (fontFamily) {
+            ctx.fontFamily = fontFamily;
+        }
 
-		if (fontSize) {
-			ctx.fontSize = fontSize;
-		}
+        if (fontSize) {
+            ctx.fontSize = fontSize;
+        }
 
-		ctx.fillText(child.textContent, child.getAttribute('x'), child.getAttribute('y'));
-		return;
-  	}
+        ctx.fillText(child.textContent, child.getAttribute('x'), child.getAttribute('y'));
+        return;
+    }
 
-	if (child.nodeName === 'polygon') {
-		var pts = child.getAttribute('points')
+    if (child.nodeName === 'polygon') {
+        var pts = child.getAttribute('points')
 
-		var opts = getFillStroke(child);
-		rc.path(`M${pts}Z`, opts);
+        var opts = getFillStroke(child);
+        rc.path(`M${pts}Z`, opts);
 
-		return;
-	}
+        return;
+    }
 
-	if (child.nodeName === 'g') {
-		var transform = child.getAttribute('transform');
-		ctx.save();
+    if (child.nodeName === 'g') {
+        var transform = child.getAttribute('transform');
+        ctx.save();
 
-		if (transform) {
-	  		var scale = /scale\(([^)]*)\)/.exec(transform);
-	  		if (scale) {
-				var args = scale[1].split(' ').map(parseFloat);
-				ctx.scale(...args);
-	  		}
+        if (transform) {
+            var scale = /scale\(([^)]*)\)/.exec(transform);
+            if (scale) {
+                var args = scale[1].split(' ').map(parseFloat);
+                ctx.scale(...args);
+            }
 
-			var rotate = /rotate\(([^)]*)\)/.exec(transform);
-			if (rotate) {
-				var args = rotate[1].split(' ').map(parseFloat);
-				ctx.rotate(...args);
-			}
+            var rotate = /rotate\(([^)]*)\)/.exec(transform);
+            if (rotate) {
+                var args = rotate[1].split(' ').map(parseFloat);
+                ctx.rotate(...args);
+            }
 
-			var translate = /translate\(([^)]*)\)/.exec(transform);
-			if (translate) {
-				var args = translate[1].split(' ').map(parseFloat);
-				ctx.translate(...args);
-			}
-		}
+            var translate = /translate\(([^)]*)\)/.exec(transform);
+            if (translate) {
+                var args = translate[1].split(' ').map(parseFloat);
+                ctx.translate(...args);
+            }
+        }
 
-		[...child.children].forEach(traverseSvgToRough);
+        [...child.children].forEach(traverseSvgToRough);
 
-		ctx.restore();
-		return;
-	}
+        ctx.restore();
+        return;
+    }
 }
 
 var pending;
-function scheduleUpdate() {
-	if (pending) clearTimeout(pending);
 
-	pending = setTimeout(() => {
-		pending = null;
-		update();
-	}, 200);
+function scheduleUpdate() {
+    if (pending) clearTimeout(pending);
+
+    pending = setTimeout(() => {
+        pending = null;
+        update();
+    }, 200);
 }
 
 // startup
-var topo;
-
-if (window.location.hash.length > 1) {
-	try {
-		topo = atob(window.location.hash.substr(1));
-	} catch {
-		console.log("Can not read topo from url hash");
-		window.location.hash = "";
-	}
-}
+// var topo;
+//
+// if (window.location.hash.length > 1) {
+//     try {
+//         topo = atob(window.location.hash.substr(1));
+//     } catch {
+//         console.log("Can not read topo from url hash");
+//         window.location.hash = "";
+//     }
+// }
 
 // if (!topo) {
 // 	topo = sessionStorage.getItem(STORAGE_KEY);
